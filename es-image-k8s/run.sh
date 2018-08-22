cf75bd60d6e3d2bf9e164a8444e2fd4c03df406c
Heaven <djyesu@gmail.com>
2018-08-23 01:28:01 +0800

    a

--- es-image-k8s/run.sh
+++ es-image-k8s/run.sh
@@ -10,9 +10,6 @@ export MINIMUM_MASTER_NODES=${MINIMUM_MASTER_NODES:-2}
 chown -R elasticsearch:elasticsearch /data
 chmod +x ./bin/elasticsearch_logging_discovery 
 ./bin/elasticsearch_logging_discovery >> ./config/elasticsearch.yml
-#ls -ld  /usr/share/elasticsearch/bin/elasticsearch-plugin
-#chmod 777  /usr/share/elasticsearch/bin/elasticsearch-plugin
-#exec su elasticsearch -c /usr/share/elasticsearch/bin/elasticsearch-plugin install --batch repository-s3 
 mkdir /backup
 chmod 777 /backup
 exec su elasticsearch -c /usr/local/bin/docker-entrypoint.sh
